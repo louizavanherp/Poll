477c7b845421fb52ffeb76be8bb097725734927f
louizavanherp <louiza.vanherp@gmail.com>
2018-03-28 17:39:15 +0200

    made function to get value from createpoll and put it in index

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,10 +8,14 @@ var primus = Primus.connect(url, {
 });
 
 primus.on('data', function(data){
-    var title = document.querySelector(".pollQuestion");
+    var title = document.querySelector(".titlePoll");
+    var answer1 = document.querySelector(".answer01");
+    var answer2 = document.querySelector(".answer02");
     //Nakijken of titel bestaat op pagina
     if(title){
-        console.log(data);
+        title.innerHTML = data.question;
+        answer1.innerHTML = data.answer1;
+        answer2.innerHTML = data.answer2;
     }
 });
 
@@ -19,9 +23,9 @@ primus.on('data', function(data){
 document.querySelector('.submitBtn').addEventListener("click", function(e){
     console.log('geklikt');
     primus.write({ 
-        question: document.querySelector('.pollQuestion'), 
-        answer1: document.querySelector('.answer1'),
-        answer2: document.querySelector('.answer2'),
+        question: document.querySelector('.pollQuestion').value, 
+        answer1: document.querySelector('.answer1').value,
+        answer2: document.querySelector('.answer2').value,
         });
     e.preventDefault();
 });
